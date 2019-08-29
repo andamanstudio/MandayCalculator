f284ad706f533ea48e5add33ff47dfea666a7754
topspinpp <andamanstudios@gmail.com>
2019-08-29 18:50:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ manHours, setManHours ] = useState("0")
+  const [ storyPoint, setStoryPoint ] = useState("0")
+  const calculateRate = (manHours) => {
+    setStoryPoint(manHours/8);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          คำนวน Manday | Andaman Studio <br />
+          เทียบบัญญัติไตรยางศ์ => 1 วัน (8 ชั่วโมง ) = 1 Manday
+          <hr />
+          ชั่วโมง/นาที ที่คุณทำงานได้ {manHours} <br />
+          ผลการคำนวณ {storyPoint} Manday 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         ใส่ ชั่วโมงที่ทำงานได้ <input type="text" onChange={e => setManHours(e.target.value)} />
+         <button onClick={() => calculateRate(manHours)}> คำนวณ </button>
       </header>
     </div>
   );
